c8e8efc2a1a6b4bbfc901c64c26902003d04051b
malaysiacopy <142707293+malaysiacopy@users.noreply.github.com>
2023-09-02 13:01:53 +0600

    test

---
+++ script.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js"
+import { getDatabase, ref, push } 
+from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js"
+
+const firebaseConfig = {
+   databaseURL: "https://playgroundcopycat-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
+
+const passportNumberEl = document.getElementById('passport')
+const stickerNumberEl = document.getElementById('sticker')
+const refNumberEl = document.getElementById('refNumber')
+
+const saveDataEl = document.getElementById('saveData')
+
+
+saveDataEl.addEventListener('click', function (){
+    push(ref(database, `Carts/${passportNumberEl.value}`), { 
+        applicationStatus : "APPROVED", 
+        passportNumber: passportNumberEl.value,
+        referenceNumber: refNumberEl.value,
+        placeOfIssue: "Dhaka,Bangladesh",
+        visaValidity: "24 Aug 2023 until 24 Nov 2023",
+        visaStatus: "VALID"
+    })
+})
