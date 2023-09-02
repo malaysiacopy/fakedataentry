8e6dafeb4d0044f5966bd7182187ae8878376ce7
malaysiacopy <142707293+malaysiacopy@users.noreply.github.com>
2023-09-02 16:22:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js"
-import { getDatabase, ref, push } 
+import { getDatabase, ref, child, set } 
 from "https://www.gstatic.com/firebasejs/9.20.0/firebase-database.js"
 
 const firebaseConfig = {
